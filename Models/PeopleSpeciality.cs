8ea7a8699f6a665541c8e886d2b200cebd588ef8
ashley <ashleybrookes86@gmail.com>
2023-04-05 21:24:32 +0100

    speciality and peoplespeciality data models

---
+++ Models/PeopleSpeciality.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Models;
+
+public class PeopleSpeciality
+{
+    [Required]
+    public int Id { get; set; }
+    
+    [Required]
+    public int SpecialityId { get; set; }
+
+    [Required]
+    public int PersonId { get; set; }
+}
